5b6f8e98d6f1c9063e9b69f3831b3302547730f5
shreya-ssalian <ssalianshreya841@gmail.com>
2026-06-02 11:58:19 +0530

    added faq controller

--- controllers/faqController.js
+++ controllers/faqController.js
@@ -1,20 +1,18 @@
-//CREATE CURD APIS 
-
 const Question = require("../models/questionModel");
 const Answer = require("../models/answerModel");
 
 // GET FULL FAQ DATA
 const getFAQ = async (req, res) => {
     try {
-        const questions = await Question.find()
+        const questions = await Question.find() //get all question stored in the DB
             .populate("user_id", "name")
             .populate("tag_id", "tag_name");
 
         const faqs = await Promise.all(
-            questions.map(async (q) => {
+            questions.map(async (q) => {//fetches the answer (related ones)
                 const answers = await Answer.find({
                     question_id: q._id
-                }).sort({ up_votes: -1 });
+                }).sort({ up_votes: -1 });//in descending order that is highest votes comes first
 
                 return {
                     question: q,
@@ -23,7 +21,7 @@ const getFAQ = async (req, res) => {
             })
         );
 
-        res.json(faqs);
+        res.json(faqs);//send it
 
     } catch (err) {
         res.status(500).json({ message: err.message });
